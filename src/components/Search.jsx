0038eba13698157163922ef1750b65b5e076f4f7
Shubham <Shubhambatra350@gmail.com>
2022-12-15 22:03:51 +0530

    search 2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,100 +1,105 @@
-import React from "react";
-import { useState } from "react";
-import { collection, query, where, getDoc, getDocs, doc, setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
-import { async } from "@firebase/util";
-import { useContext } from "react";
-import { AuthContext } from '../context/AuthContext'
-
-const Search = () =>{
-    const [username, setUsername] = useState("");
-    const [user, setUser] = useState(null);
-    const [err, setErr] =  useState(false);
-    const currentUser = useContext(AuthContext);
+import { AuthContext } from "../context/AuthContext";
 
 
-    //search function
-    const handleSearch = async () => {
-        const q = query(collection(db, "users"), 
-        where("displayName", "==", username), 
-        // where("uid", "!=", currentUser.uid),
-        );
+const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-        try{
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                setUser(doc.data());
-                console.log(doc.data())
-            });
-        } catch(err){
-            setErr(true);
-        }
-    };
+  const { currentUser } = useContext(AuthContext);
 
-    //search function
-    const handleKey = (e) => {
-        e.code === "Enter" && handleSearch();
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
     }
+  };
 
-    const handleSelect = async () => {
-        const combinedId = 
-        currentUser.uid > username.uid? 
-        currentUser.uid + user.uid:
-        user.uid + currentUser.uid;
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
 
-        try{
-            const res = await getDoc(db, "chats", combinedId);
+  const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
 
-            if(!res.exists()){
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-                //create chats in chat collection
-                await setDoc(doc, (db, "chats", combinedId), {messages: [] });
-                
-                //create user chats
-                    //user 1
-                await updateDoc(doc, (db, "userChats", currentUser.uid),{
-                    [combinedId + ".userInfo"]:{
-                        uid: user.uid,
-                        displayName: user.displayName,
-                        photoURL: user.photoURL
-                    },
-                    [combinedId + ".date"]: serverTimestamp()
-                });
-                    //user 2
-                await updateDoc(doc, (db, "userChats", user.uid),{
-                    [combinedId + ".userInfo"]:{
-                        uid: currentUser.uid,
-                        displayName: currentUser.displayName,
-                        photoURL: currentUser.photoURL
-                    },
-                    [combinedId + ".date"]: serverTimestamp()
-                });
-            }
-        } catch (err) {}
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
 
-        setUser(null);
-        setUsername("")
-    }
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
 
-    return(
-        <div className="search">
-            <div className="searchForm">
-                <input type="text" 
-                placeholder="find a user" 
-                onKeyDown={handleKey} 
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-                />
-            </div>
-            {err && <span >User not found</span>}
-            { user && (<div className="userChat" onClick={handleSelect}>
-                <img src={user.photoURL} alt="" />
-                <div className="userChatInfo">
-                    <span>{user.displayName}</span>
-                </div>
-            </div>)}
+    setUser(null);
+    setUsername("")
+  };
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Search;
